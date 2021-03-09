b109841d5249aa14f8fb8c674bc2d6acc0affc52
AnkitaL <DHRUV SHELADIA@192.168.1.206>
2021-03-09 17:31:07 +0530

    third commit [Add Eureka server]

---
+++ spring-serviceregistry/src/test/java/com/springboot/ashel/SpringServiceregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.ashel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServiceregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
